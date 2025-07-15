8435c56e12e4f03f78975ed0d0d675d147a06326
ctrlcat0x <developer.sahilrana@gmail.com>
2025-07-15 16:25:58 +0530

    Refactor Prisma schema and implement messaging functionality
    - Introduced new Message and Fragment models in schema.prisma to support messaging features.
    - Added MessageRole and MessageType enums for better message categorization.
    - Removed obsolete User and Post models along with related seed data and migration files.
    - Updated page.tsx to utilize TRPC for fetching messages and creating new ones.
    - Refactored Inngest functions to handle message creation and error reporting in the database.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 import { useState } from "react";
 import { useTRPC } from "@/trpc/client";
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQuery } from "@tanstack/react-query";
 import { toast } from "sonner";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
@@ -9,10 +9,11 @@ import { Button } from "@/components/ui/button";
 const Page = () => {
   const [value, setValue] = useState("");
   const trpc = useTRPC();
-  const invoke = useMutation(
-    trpc.invoke.mutationOptions({
+  const { data: messages } = useQuery(trpc.messages.getMany.queryOptions());
+  const createMessage = useMutation(
+    trpc.messages.create.mutationOptions({
       onSuccess: () => {
-        toast.success("Background job started");
+        toast.success("Message started");
       },
     })
   );
@@ -20,11 +21,12 @@ const Page = () => {
     <div className="p-4 max-w-7xl mx-auto">
       <Input value={value} onChange={(e) => setValue(e.target.value)} />
       <Button
-        disabled={invoke.isPending}
-        onClick={() => invoke.mutate({ value: value })}
+        disabled={createMessage.isPending}
+        onClick={() => createMessage.mutate({ value: value })}
       >
         Invoke Job
       </Button>
+      {JSON.stringify(messages, null, 2)}
     </div>
   );
 };
